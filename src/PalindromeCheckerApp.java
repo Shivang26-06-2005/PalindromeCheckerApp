e5fa54d2b19a919e71cbc7462fbcfd0566d33ac3
Shivang26-06-2005 <shivangthakkar2611@gmail.com>
2026-02-12 10:26:16 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+/**
+ * ===============================================================
+ * MAIN CLASS - UseCase1PalindromeApp
+ * ===============================================================
+ *
+ * Palindrome Checker Management System
+ *
+ * Version 1.0
+ */
+public class PalindromeCheckerApp {
+   public static void main(String[] args) {
+       System.out.println("Welcome to the Palindrome Checker Management system");
+       System.out.println("Version : 1.0");
+       System.out.println("System initialized successfully");
+
+    }
+
+
+}
