caf60d1d2a5175fd4f658a9fba45a67dbbe9ee40
satvikch859 <nc5055@srmist.edu.in>
2026-02-19 10:14:51 +0530

    Shivang  Added Usecase2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,22 @@
-/**
- * ===============================================================
- * MAIN CLASS - UseCase1PalindromeApp
- * ===============================================================
- *
- * Palindrome Checker Management System
- *
- * Version 1.0
- */
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-   public static void main(String[] args) {
-       System.out.println("Welcome to the Palindrome Checker Management system");
-       System.out.println("Version : 1.0");
-       System.out.println("System initialized successfully");
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Text:");
+        String s1=sc.nextLine();
+        int length=s1.length();
+        boolean pal=false;
+        for(int i=0;i<length/2;i++){
+            if(s1.charAt(i)==s1.charAt(length-i-1)){
+                pal=true;
+            }
+        }
+        if(pal){
+           System.out.println("Is it a palindrome?"+" "+pal);
+        }
 
     }
 
 
-}
+
+    }
